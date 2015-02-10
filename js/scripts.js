3637479cd52f3c736b106ea1583a8331dc09620d
Debbie Cravey and Jake Kaad <kaadalac@gmail.com>
2015-02-10 15:41:59 -0800

    add functionality for comparing arrays and returning matches

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,47 @@
-var anagram = function() {
-  return "Please enter words";
+var anagram = function(string, arrayToCompare) {
+  var anagrams = getAnagramArray(string, arrayToCompare);
+  if (hasMatch(string, arrayToCompare)) {
+    return "Matches: " + anagrams.join(", ");
+  } else {
+    return "No anagrams found";
+  }
+};
+
+var hasMatch = function(string, arrayToCompare) {
+  var stringLetters = string.split("").sort();
+  var anagrams = [];
+  arrayToCompare.forEach(function(word){
+    var wordLetters = word.split("").sort();
+    if (checkArrays(stringLetters, wordLetters)) {
+      anagrams.push(word);
+    }
+  });
+  if (!!anagrams[0]) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+
+var getAnagramArray = function(string, arrayToCompare) {
+  var stringLetters = string.split("").sort();
+  var anagrams = [];
+  var wordLetters = [];
+  arrayToCompare.forEach(function(word){
+    wordLetters = word.split("").sort();
+    if (checkArrays(stringLetters, wordLetters)) {
+      anagrams.push(word);
+    }
+  });
+  return anagrams;
+};
+
+var checkArrays = function(arr1, arr2) {
+  for(var i = 0; i < (arr1.length + 1); i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  };
+  return true;
 };
--- specs/specs.js
+++ specs/specs.js
@@ -1,5 +1,19 @@
 describe('anagram', function() {
-  it("fails if the input is not a string of letters", function() {
-    expect(anagram("$$&^")).to.equal("Please enter words");
+  it("returns 'No anagrams found' if no anagrams are found", function() {
+    expect(anagram("hi", ["no", "yes", "up", "down"])).to.equal("No anagrams found")
+  });
+
+  it("returns matching anagrams from the array of the word", function() {
+    expect(anagram("arc", ["car", "dog", "cat", "cap", "hot"])).to.equal("Matches: car")
+  });  
+});
+
+describe('checkArrays', function() {
+  it("is true if the arrays match", function() {
+    expect(checkArrays(["a", "c", "r"], ["a", "c","r"])).to.equal(true);
+  });
+
+  it("is false if the arrays dont match", function(){
+    expect(checkArrays(["a", "c", "r"], ["b","t","r"])).to.equal(false);
   });
 });
